9325ba6d2d10bf9b1eaba2fb0d6258418749eeb6
mayankjonwal02 <jonwal.1@iitj.ac.in>
2024-07-21 15:40:49 +0530

    ip

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b&b^z@gd20s$@-(v%$g^__j&g!ka7anb@h13g3bc@6d)taq2&-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["*"]
+
+ALLOWED_HOSTS = ['10.1.1.4','localhost']
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
