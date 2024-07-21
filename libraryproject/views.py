a1a924fb9b561c42d8e8651a546e7e169c87f6b9
mayankjonwal02 <jonwal.1@iitj.ac.in>
2024-07-22 00:01:32 +0530

    functions added

--- libraryproject/views.py
+++ libraryproject/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 
 def developedby(request):
     return render(request, 'developedby.html')
+
+def registerAPI(request):
+    pass
+
+def verifyAPI(request):
+    pass
+
+def deregisterAPI(request):
+    pass
+
+def registrationPage(request):
+    pass
+
+def verifyPage(request):
+    pass
+
+def deregisterPage(request):
+    pass
